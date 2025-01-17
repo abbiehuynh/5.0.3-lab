71f155e758ba9673c9293aaa101060b07ec4bea6
abbiehuynh <abbiehuynhh@gmail.com>
2025-01-16 18:23:57 -0600

    Answers lab questions 1-4

--- src/main/java/academy/javapro/lab/OutputFormatting.java
+++ src/main/java/academy/javapro/lab/OutputFormatting.java
@@ -12,18 +12,23 @@ public class OutputFormatting {
 
         // 1. Use printf() to format and print a person's name in a field width of 20 characters, left-aligned.
         System.out.println("Name formatting:");
+        System.out.printf("%-20s\n", name);
 
         // 2. Implement printf() to display an integer (age) in a field width of 5 characters, right-aligned.
         System.out.println("\n Age formatting:");
+        System.out.printf("%5d\n", age);
 
         // 3. Format a double value (salary) with 2 decimal places and thousands separator using printf().
         System.out.println("\n Salary formatting:");
+        System.out.printf("%,.2f\n", salary);
 
         // 4. Use printf() to print the current date in the format: MM/dd/yyyy.
         System.out.println("\n Date formatting:");
+        System.out.printf("%tm/%td/%tY\n", currentDate, currentDate, currentDate);
 
         // 5. Create formatted strings using String.format() for combining name and age, and for representing salary as currency.
         System.out.println("\n Formatted strings:");
+
 
         // 6. Format and print a table of sample data for 3 people using printf() to align columns properly.
         System.out.println("\n Formatted table:");
